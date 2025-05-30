f7e2c34a642b5b007e2b3fd4af659ce5c229431f
Francis Tedeus <francistedeus@gmail.com>
2025-05-30 17:36:26 -0400

    Adding Sqlite

--- Models/InventoryItem.cs
+++ Models/InventoryItem.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LogiTrack.Models
 {
     public class InventoryItem
     {
+        [Key]
         public int ItemId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int Quantity { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Location { get; set; }
 
         public string DisplayInfo()
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LogiTrack.Models
 {
     public class Order
     {
+        [Key]
         public int OrderId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string CustomerName { get; set; }
+
+        [Required]
         public DateTime DatePlaced { get; set; }
+
+        [Required]
         public List<InventoryItem> Items { get; set; }
     }
 }
